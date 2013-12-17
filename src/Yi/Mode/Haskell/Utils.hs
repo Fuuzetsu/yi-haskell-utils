9b1d96203b3e7f6586c73ded176ba0150e94deb2
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-12-17 03:31:27 +0000

    initial setup

---
+++ src/Yi/Mode/Haskell/Utils.hs
@@ -0,0 +1,9 @@
+-- |
+-- Module      :  Yi.Mode.Haskell.Utils
+-- Copyright   :  (c) Mateusz Kowalczyk 2013
+-- License     :  GPL-3
+--
+-- Maintainer  :  fuuzetsu@fuuzetsu.co.uk
+-- Stability   :  experimental
+
+module Yi.Mode.Haskell.Utils
